fa763a7b5a885a3739086e1025427eb010dc9fd9
0x61B6 <86880403+0x61B6@users.noreply.github.com>
2022-09-22 16:55:27 +0300

    adjusted some filters

--- greentext.py
+++ greentext.py
@@ -5,56 +5,58 @@ def green_text():
     import os
     import requests
     from main import sub_reddit
-    for post in sub_reddit.new(limit=1):
+    for post in sub_reddit.hot(limit=5):
         print(post.title, post.url)
-        statuses = main.api1.user_timeline(count=1, include_rts=False, exclude_replies=True)
+        statuses = main.api1.user_timeline(count=20, include_rts=False, exclude_replies=True)
         if hasattr(post, "is_gallery"):
             print("Gallery post, sleeping.")
             time.sleep(300)
         else:
-            print("Fetched statuses for checking post.title duplicate")
+            print(f"Fetched {len(statuses)} statuses for checking post duplicate")
+            status_list = []
             for status in statuses:
-                if post.title.lower() in status.text.lower():
-                    print("False")
-                    time.sleep(200)
-                else:
+                status_list.append(status.text.lower())
+            if post.title.lower() in status_list:
+                print("False")
+                time.sleep(200)
+            else:
+                try:
+                    print("True")
+                    url = post.url
+                    r = requests.get(url, allow_redirects=True)
+                    print(r)
+                    open('sample2.jpg', 'wb').write(r.content)
+                    # response = b.shorten(post.permalink)
+                    this2 =  main.api1.update_status_with_media(
+                        status=post.title, filename='sample2.jpg')
+                    print("Posted")
+                    os.remove('sample2.jpg')
+
+                    time.sleep(10)
                     try:
-                        print("True")
-                        url = post.url
-                        r = requests.get(url, allow_redirects=True)
-                        print(r)
-                        open('sample2.jpg', 'wb').write(r.content)
-                        # response = b.shorten(post.permalink)
-                        this2 =  main.api1.update_status_with_media(
-                            status=post.title, filename='sample2.jpg')
-                        print("Posted")
-                        os.remove('sample2.jpg')
+                        that2 = main.c_api1.update_status(status="reddit.com" + post.permalink, 
+                        in_reply_to_status_id=this2.id, auto_populate_reply_metadata=True)
+                        print("replied")
+                    except Exception as shid:
+                        print(shid)
+                        print("couldn't reply")
 
-                        time.sleep(10)
-                        try:
-                            that2 = main.c_api1.update_status(status="reddit.com" + post.permalink, 
-                         in_reply_to_status_id=this2.id, auto_populate_reply_metadata=True)
-                            print("replied")
-                        except Exception as shid:
-                            print(shid)
-                            print("couldn't reply")
-
-                        '''
-                        time.sleep(2)
-                        try:
-                            main.apy2.hidden_reply(tweet_id=that2.id, hidden=True)
-                            print("reply hidden successfuly")
+                    '''
+                    time.sleep(2)
+                    try:
+                        main.apy2.hidden_reply(tweet_id=that2.id, hidden=True)
+                        print("reply hidden successfuly")
                             
-                        except Exception as repl:
-                             print(repl)
-                             print("couldn't hide reply")   
-                        '''
+                    except Exception as repl:
+                        print(repl)
+                        print("couldn't hide reply")   
+                    '''
                         
-                        time.sleep(799)
-                    except Exception as bruh_moment:
-                        print(bruh_moment)
-                        print("sleeping for 69 secs")
-                        time.sleep(69)
-                        green_text()
+                    time.sleep(799)
+                except Exception as bruh_moment:
+                    print(bruh_moment)
+                    print("sleeping for 69 secs")
+                    time.sleep(69)
+                    green_text()
     
     start()
--- historymemes.py
+++ historymemes.py
@@ -5,58 +5,61 @@ def history_memes():
     import os
     import requests
     from main import subreddit
-    for post in subreddit.new(limit=1):
+    for post in subreddit.hot(limit=5):
         print(post.title, post.url)
-        statuses = main.spi.user_timeline(count=1, include_rts=False, exclude_replies=True)
+        statuses = main.spi.user_timeline(count=20, include_rts=False, exclude_replies=True)
         if hasattr(post, "is_gallery"):
             print("Gallery post, sleeping.")
             time.sleep(300)
         else:
-            print("Fetched statuses for checking post.title duplicate")
+            print(f"Fetched {len(statuses)} statuses for checking post duplicate")
+            status_list = []
             for status in statuses:
-                if post.title.lower() in status.text.lower():
-                    print("False")
-                    time.sleep(201)  
-                else:
+                status_list.append(status.text.lower())
+            processed_title = post.title.lower() + "\n" + "#meme #history"
+            if processed_title in status_list:
+                print("False")
+                time.sleep(201)  
+            else:
+                try:
+                    print("True")
+                    url = post.url
+                    r = requests.get(url, allow_redirects=True)
+                    print(r)
+                    open('sample1.jpg', 'wb').write(r.content)
+                    # response = b.shorten(post.permalink)
+                    this = main.spi.update_status_with_media(
+                        status=post.title + "\n" + "#meme #history",
+                        filename='sample1.jpg')
+                    print("Posted")
+                    os.remove('sample1.jpg')
+                         
+                    time.sleep(10)
                     try:
-                        print("True")
-                        url = post.url
-                        r = requests.get(url, allow_redirects=True)
-                        print(r)
-                        open('sample1.jpg', 'wb').write(r.content)
-                        # response = b.shorten(post.permalink)
-                        this = main.spi.update_status_with_media(
-                            status=post.title + "\n" + "#meme #history",
-                            filename='sample1.jpg')
-                        print("Posted")
-                        os.remove('sample1.jpg')
-                         
-                        time.sleep(10)
-                        try:
-                            that = main.c_api1.update_status(status="reddit.com" + post.permalink, 
-                         in_reply_to_status_id=this.id, auto_populate_reply_metadata=True)
-                            print("replied")
-                        except Exception as shid:
-                            print(shid)
-                            print("couldn't reply")
+                        that = main.c_api1.update_status(status="reddit.com" + post.permalink, 
+                        in_reply_to_status_id=this.id, auto_populate_reply_metadata=True)
+                        print("replied")
+                    except Exception as shid:
+                        print(shid)
+                        print("couldn't reply")
 
-                        ''' 
-                        time.sleep(2)
-                        try:
-                            main.apy.hidden_reply(tweet_id=that.id, hidden=True)
-                            print("reply hidden successfuly")
+                    ''' 
+                    time.sleep(2)
+                    try:
+                        main.apy.hidden_reply(tweet_id=that.id, hidden=True)
+                        print("reply hidden successfuly")
                             
-                        except Exception as repl:
-                             print(repl)
-                             print("couldn't hide reply")
-                        '''
+                    except Exception as repl:
+                        print(repl)
+                        print("couldn't hide reply")
+                    '''
                          
-                        time.sleep(799)
-                    except Exception as bruh_moment:
-                        print(bruh_moment)
-                        print("sleeping for 69 secs")
-                        time.sleep(69)
-                        history_memes()
+                    time.sleep(799)
+                except Exception as bruh_moment:
+                    print(bruh_moment)
+                    print("sleeping for 69 secs")
+                    time.sleep(69)
+                    history_memes()
                         
                         
     green_text()                   
